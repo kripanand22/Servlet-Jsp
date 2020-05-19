27a78ba6a46ded1804c241520e9b2bee718739b9
kripanand <kripanand22@gmail.com>
2020-05-19 23:09:53 +0530

    Add files via upload

---
+++ 01First/WEB-INF/classes/AddServlet.java
@@ -0,0 +1,34 @@
+import javax.servlet.*;
+import java.io.*;
+
+public class AddServlet implements Servlet{
+	public void init(ServletConfig con){
+	}
+	public void service(ServletRequest req,ServletResponse res){
+		PrintWriter out=null;
+		try{
+			res.setContentType("text/html");
+			out=res.getWriter();
+			String s1=req.getParameter("num1");
+			String s2=req.getParameter("num2");
+			int n1=Integer.parseInt(s1);
+			int n2=Integer.parseInt(s2);
+			int n=n1+n2;
+			out.println("<html><body>");
+			out.println("<h1>First Web App</h1>");
+			out.println("<hello Servlet<br>");  
+			out.println("Sum of<b> "+n1+" </b>and<b> "+n2+" </b>=<b>"+n+"</b>");
+			out.println("</body></html>");
+		}catch(Exception e){
+				out.println(e);
+		}
+	}
+		public void destroy(){}
+		public ServletConfig getServletConfig(){
+			return null;
+		}
+		public String getServletInfo(){
+			return null;
+		}
+	
+}
